8aaca25206538397b49cc1a1f5d3880166ded1c5
Ryan Conrad G. Salazar <30727670+Ubuntuify@users.noreply.github.com>
2024-09-11 08:53:53 +0800

    refactor: moved home page out of main app Scaffold.
    Signed-off-by: Ryan Conrad G. Salazar <30727670+Ubuntuify@users.noreply.github.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:schedule/ui/home.dart';
+import 'package:schedule/ui/app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,47 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:schedule/schema/schedule.dart';
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+class Home extends StatefulWidget {
+  const Home({super.key});
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  State<Home> createState() => _HomeState();
 }
 
-class _HomePageState extends State<HomePage> {
-  int navigationIndex = 0;
-
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return PopScope(
-      // PopScope is setup for predictive back gestures on Android
-      canPop: true,
-      child: Scaffold(
-        body: const Card(),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () => {},
-          child: const Icon(Icons.add),
-        ),
-        bottomNavigationBar: NavigationBar(
-          onDestinationSelected: (int index) =>
-              setState(() => navigationIndex = index),
-          destinations: const [
-            NavigationDestination(
-                icon: Icon(Icons.home_outlined),
-                selectedIcon: Icon(Icons.home),
-                label: 'Home'),
-            NavigationDestination(
-                icon: Icon(Icons.calendar_month_outlined),
-                selectedIcon: Icon(Icons.calendar_month),
-                label: 'Classes'),
-            NavigationDestination(
-                icon: Icon(Icons.settings_outlined),
-                selectedIcon: Icon(Icons.settings),
-                label: 'Settings')
-          ],
-          selectedIndex: navigationIndex,
-          labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
-        ),
-      ),
-    );
+    return const Placeholder();
+  }
+
+  Class _getNextClass() {
+    throw UnimplementedError(
+        'getNextClass private function not yet implemented.');
   }
 }
